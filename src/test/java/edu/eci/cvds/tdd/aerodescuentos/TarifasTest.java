3972eaf52b6e7791ab6418dff82401eaafc54bac
JuanDpr99 <juan.parroquiano@mail.escuelaing.edu.co>
2024-02-10 13:48:50 -0500

    avance parte final

---
+++ src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -0,0 +1,17 @@
+package edu.eci.cvds.tdd.aerodescuentos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TarifasTest
+{
+    private CalculadorDescuentos c= new CalculadorDescuentos();
+    private double tarifa;
+
+    @Test
+    public void validarEdadMenor0(){
+
+        Assert.assertTrue(true);
+    }
+
+}
